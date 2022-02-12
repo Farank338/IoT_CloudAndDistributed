6f1fb86f3392b1cb92cd0e8003941b7faecaf706
Farank338 <you@example.com>
2022-02-12 22:40:11 +0300

    1

--- HW2/entrypoint.sh
+++ HW2/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
+#Фактически это не старт а перезапуск который нужен чтобы ранее сделанная настройка на прослушку всех адресов заработала
 service postgresql start &&\
+
+#Обновим репозиторий с решением нужно для того чтобы подкачались свежайшие исходники с кодом
 rm -R IoT_CloudAndDistributed &&\
 git clone https://github.com/Farank338/IoT_CloudAndDistributed.git &&\
 cd IoT_CloudAndDistributed &&\
 cd HW2 &&\
+
+#Запускаем основной срипт
 chmod +x run.sh 
 ./run.sh
+
