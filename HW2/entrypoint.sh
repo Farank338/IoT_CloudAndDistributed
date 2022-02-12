b92037d3fc8c6c0f62bce6f48bb6fe008a3a4423
Farank338 <you@example.com>
2022-02-12 19:51:50 +0300

    1

--- HW2/entrypoint.sh
+++ HW2/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #/etc/init.d/postgresql start &&\
-#pg_ctl start
+
 service postgresql start &&\
+pg_ctl listen_addresses '*' &&\
 rm -R IoT_CloudAndDistributed &&\
 git clone https://github.com/Farank338/IoT_CloudAndDistributed.git &&\
 cd IoT_CloudAndDistributed &&\
