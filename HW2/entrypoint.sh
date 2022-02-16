3495069e7bcde3e3bf3a9bd056e8874085e1ed23
Farank338 <53602737+Farank338@users.noreply.github.com>
2022-02-16 12:53:57 +0300

    Update entrypoint.sh

--- HW2/entrypoint.sh
+++ HW2/entrypoint.sh
@@ -8,6 +8,7 @@ rm -R IoT_CloudAndDistributed &&\
 git clone https://github.com/Farank338/IoT_CloudAndDistributed.git &&\
 cd IoT_CloudAndDistributed &&\
 cd HW2 &&\
+chmod +x entrypoint.sh
 
 #Запускаем основной срипт
 chmod +x run.sh 
