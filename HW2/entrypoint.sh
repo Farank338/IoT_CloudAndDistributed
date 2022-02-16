4f45d0d2018bccaa2b208f0e8005c31c3bf11ad1
Ваше Имя <you@example.com>
2022-02-16 18:52:08 +0300

    1

--- HW2/entrypoint.sh
+++ HW2/entrypoint.sh
@@ -9,6 +9,21 @@ git clone https://github.com/Farank338/IoT_CloudAndDistributed.git &&\
 cd IoT_CloudAndDistributed &&\
 cd HW2 &&\
 chmod +x entrypoint.sh
+function is_in_activation {
+   activation=$(systemctl status postgresql | grep "Active: active" )  
+   if [ -z "$activation" ]; then
+      true;
+   else
+      false;
+   fi
+
+   return $?;
+}
+
+while is_in_activation network;       
+    do true    
+    done    
+
 
 #Запускаем основной срипт
 chmod +x run.sh 
